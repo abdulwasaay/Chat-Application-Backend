1773df14f6ce4a6532cb89c3d86d3c1d5ea3b1cd
= <=wasaysaleem071@gmail.com>
2024-10-14 00:00:55 +0500

    done

--- Config/CookieConfig.js
+++ Config/CookieConfig.js
@@ -1,7 +1,10 @@
+require('dotenv').config({ path: "./Config/.env" })
+
 const cookieConfig = {
     maxAge: 2592000000,
-        secure: true,
-        // sameSite: 'strict',
+    secure: true,
+    sameSite: 'none',
+    path: process.env.ORIGIN
 }
 
 module.exports = cookieConfig
